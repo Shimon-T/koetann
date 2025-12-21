05a7fb934ef5a3e355f4efe8cf073464c0f85020
Shimon-T <shimontk11165478@gmail.com>
2025-12-21 14:57:14 +0900

    Initial Commit

---
+++ koetann/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  koetann
+//
+//  Created by 田中志門 on 12/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ koetann/koetannApp.swift
@@ -0,0 +1,17 @@
+//
+//  koetannApp.swift
+//  koetann
+//
+//  Created by 田中志門 on 12/21/25.
+//
+
+import SwiftUI
+
+@main
+struct koetannApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
